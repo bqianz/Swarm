eb1c8c98274cf0f954b2a21db2b419ccbf3911f9
Beril Zhang <moujunki@gmail.com>
2019-06-05 16:53:02 -0700

    dijkstra and curve length calculation working

--- Torus/Torus/Torus.py
+++ Torus/Torus/Torus.py
@@ -4,15 +4,14 @@ import math
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib.path import Path
+from numpy import linalg as LA
 
-def find_neighbours(current,visited,n,temp):
+def find_neighbours(current,n):
 
     i= current[0]
     j = current[1]
 
     neighbours = []
-    neighbours_th = []
-    neighbours_ph = []
 
     i_prev = i-1
     i_next = i+1
@@ -28,43 +27,47 @@ def find_neighbours(current,visited,n,temp):
     elif j == n-1:
         j_next = 0
 
-    for u in [[i_prev,j],[i_next,j],[i,j_prev],[i,j_next]]:
-        if visited[u[0],u[1]] == 0:
-            neighbours.append(u)
-            neighbours_th.append(temp[u[0]])
-            neighbours_ph.append(temp[u[1]])
-    return neighbours_th, neighbours_ph, neighbours
+    return [(i_prev,j),(i_next,j),(i,j_prev),(i,j_next)]
 
+def euclidean_dist(u,v,temp,c,a):
+    x1, y1, z1 = tor2cart(temp[u[0]],temp[u[1]],c,a)
+    x2, y2, z2 = tor2cart(temp[v[0]],temp[v[1]],c,a)
+
+    return np.sqrt((x1-x2)**2 + (y1-y2)**2 + (z1-z2)**2)
 
 def Dijkstra(n,start,end,temp):
 
-    dist = np.full((n,n), n**2)
-    prev = np.full((n,n,2),-1)
-    visited = np.zeros((n,n))
+    vertices = [(a,b) for a in range(n) for b in range(n)]
+    dist = {vertex: math.inf for vertex in vertices}
+    prev = {vertex: None for vertex in vertices}
 
-    dist[start[0],start[1]] = 0
-    visited[start[0],start[1]] = 1
+    dist[start] = 0
     current = start;
 
-    while np.prod(visited) == 0 and not np.array_equal(current,end):
-        current_dist = dist[current[0],current[1]]
-        _, _, neighbours = find_neighbours(current,visited,n,temp)
-        next_dist = n^2
-        # print("current vertex index:")
-        # print(current)
-        for v in neighbours:
-            alt = current_dist + euclidean_dist(current, v, temp, c, a)
-            if alt < dist[v[0],v[1]]:
-                dist[v[0],v[1]] = alt
-                prev[v[0],v[1]] = current
+    while vertices and current != end:
+        current = min(vertices, key=lambda vertex: dist[vertex])
+        vertices.remove(current)
+        print(len(vertices))
+        for v in find_neighbours(current,n):
+            if v in vertices:
+                alt = dist[current] + euclidean_dist(current, v, temp, c, a)
+                if alt < dist[v]:
+                    dist[v] = alt
+                    prev[v] = current
 
-        # select current with the least value of dist[current]
-        flat_min = np.argmin(dist)
-        row = flat_min // n
-        column = flat_min - row*n
-        current = [row, column]
-
-    return prev
+    current = end
+    th_g = [temp[end[0]]]
+    ph_g = [temp[end[1]]]
+    while current != start:
+        th_g.append(temp[current[0]])
+        ph_g.append(temp[current[1]])
+        current = prev[current]
+    
+    th_g.append(temp[start[0]])
+    ph_g.append(temp[start[1]])
+    th_g = np.asarray(th_g)
+    ph_g = np.asarray(ph_g)
+    return th_g, ph_g
 
 
 
@@ -74,39 +77,6 @@ def tor2cart(theta, phi, c, a):
     z = a * np.sin(theta)
     return x, y, z
 
-def euclidean_dist(u,v,temp,c,a):
-    x1, y1, z1 = tor2cart(temp[u[0]],temp[u[1]],c,a)
-    x2, y2, z2 = tor2cart(temp[v[0]],temp[v[1]],c,a)
-
-    return np.sqrt((x1-x2)**2 + (y1-y2)**2 + (z1-z2)**2)
-
-
-def test_find_neighbours():
-    n = 100
-    c, a = 2, 1
-    temp = np.linspace(0, 2*np.pi, n)
-    phi, theta = np.meshgrid(temp,temp)
-    x,y,z = tor2cart(theta,phi,c,a)
-
-    # draw the mesh
-    fig = plt.figure()
-    ax = fig.gca(projection = '3d')
-    ax.set_zlim(-3,3)
-    ax.plot_surface(x, y, z, rstride=5, cstride=5, edgecolors='w', alpha = 0.3)
-    ax.view_init(36, 26)
-    
-    # draw vertex
-    vertex = [50,0]
-    xv,yv,zv = tor2cart(temp[vertex[0]],temp[vertex[1]],c,a)
-    ax.scatter(xv, yv, zv, c='r')
-
-    # draw neighbours
-    visited = np.zeros((n,n))
-    nb_th, nb_ph, nb = find_neighbours(vertex,visited,n,temp)
-    x_nb, y_nb,z_nb = tor2cart(nb_th, nb_ph,c,a)
-    ax.scatter(x_nb,y_nb,z_nb,c='b')
-
-    plt.show()
 
 def functional_iteration(th,ph,a,c):
     ph_diff = ph[2:] - ph[0:-2]
@@ -123,13 +93,8 @@ def functional_iteration(th,ph,a,c):
 
     return new_th, new_ph
 
-if __name__ == "__main__":
-    n = 100
-    c, a = 2, 1
-    temp = np.linspace(0, 2*np.pi, n)
-    phi, theta = np.meshgrid(temp,temp)
-    x,y,z = tor2cart(theta,phi,c,a)
 
+def draw_base(x,y,z,xv,yv,zv):
     # draw the mesh
     fig = plt.figure()
     ax = fig.gca(projection = '3d')
@@ -137,33 +102,119 @@ if __name__ == "__main__":
     ax.plot_surface(x, y, z, rstride=5, cstride=5, edgecolors='none', alpha = 0.3)
     ax.view_init(36, 26)
     
-    # draw two vertices
-    start = (79,30)
-    end = (0,0)
-    xv,yv,zv = tor2cart(temp[np.array([start[0],end[0]])],temp[np.array([start[1],end[1]])],c,a)
+    # draw 
     ax.scatter(xv, yv, zv, c='r')
+    return fig, ax
+
+def initial_path(n,start,end,temp):
+    half_th = int(n/2)
+
+    th_s = start[0]
+    th_e = end[0]
+    ph_s = start[1]
+    ph_e = end[1]
+    
+    if abs(th_s - th_e) < half_th:
+        # go through half_th
+        if th_s < th_e:
+            th_seg1 = range(start[0],half_th)
+            th_seg2 = range(half_th,end[0])
+    else:
+        # go through th = 0
+        ph_seg = 0
+
+    seg1 = range(start[0],half_th)
+    seg2 = range(start[1],end[1])
+    seg3 = range(half_th,end[0])
+
+    th_ind = list(seg1) + [half_th for i in seg2] + list(seg3) + [end[0]]
+    ph_ind = [start[1] for i in seg1] + list(seg2) + [end[1] for i in seg3] + [end[1]]
+    th = temp[np.array(th_ind)]
+    ph = temp[np.array(ph_ind)]
+    return th, ph, th.size
+
+def curve_length(th,ph):
+    N = th.size - 1
+    delta = 1/N
+    delta2 = 2/N
+
+    sum = math.sqrt( ( a * (th[1] - th[0]) / delta )**2 + ( (c + a*math.cos(th[0])) * (ph[1] - ph[0]) / delta )**2)
+    sum += math.sqrt( ( a * (th[-1] - th[-2]) / delta )**2 + ( (c + a*math.cos(th[0])) * (ph[-1] - ph[-2]) / delta )**2)
+    sum = sum/2
+
+    for i in range(1,N):
+        sum += math.sqrt( ( a * (th[i+1] - th[i-1]) / delta2 )**2 + ( (c + a*math.cos(th[i])) * (ph[i+1] - ph[i-1]) / delta2 )**2)
+
+    return sum * delta
+
+def ex1():
+    var = 30
+    n = 100
+    c, a = 2, 1
+    start = (0,0)
+    end = (var,0)
+    expected_length = 2 * math.pi * a * var/100
+
+    return n, c, a, start, end, expected_length
+
+if __name__ == "__main__":
+    n = 100
+    c, a = 2, 1
+    start = (25,25)
+    end = (0,0)
+
+    expected_length = 0
+
+    # n, c, a, start, end, expected_length = ex1()
+
+
+    # plotting
+    temp_mesh = np.linspace(0, 2*np.pi, n+1)
+    temp = temp_mesh[:-1]
+    phi, theta = np.meshgrid(temp_mesh,temp_mesh)
+    xx,yy,zz = tor2cart(theta,phi,c,a)
+    xv,yv,zv = tor2cart(temp[np.array([start[0],end[0]])],temp[np.array([start[1],end[1]])],c,a)
+    fig1, ax1 = draw_base(xx,yy,zz,xv,yv,zv)
 
     # get initial path
-    prev = Dijkstra(n,start,end,temp)
-    current = end
-    th_g = [temp[end[0]]]
-    ph_g = [temp[end[1]]]
-    while not np.array_equal(current,start):
-        th_g.append(temp[current[0]])
-        ph_g.append(temp[current[1]])
-        current = prev[current[0],current[1]]
+    # th, ph, N = initial_path(n,start,end,temp)
 
-    # smooth path with fixed point iteration    
-    print(th_g)
-    print(ph_g)
-    th_g = np.asarray(th_g)
-    ph_g = np.asarray(ph_g)
-    for i in range(10000):
-        th_g, ph_g = functional_iteration(th_g,ph_g,a,c)
+    th, ph = Dijkstra(n,start,end,temp)
 
-    xg,yg,zg = tor2cart(th_g,ph_g,c,a)
-    ax.plot(xg,yg,zg,c='g')
+    print(th)
+    print(ph)
+
+    x,y,z = tor2cart(th,ph,c,a)
+    ax1.plot(x,y,z,c='g')
+    
+    # smooth path with fixed point iteration
+    count = 0
+    norm = math.inf
+    while norm > 0.0001:
+        th_new, ph_new = functional_iteration(th,ph,a,c)
+        diff_th = LA.norm(th_new - th, np.inf)
+        diff_ph = LA.norm(ph_new - ph, np.inf)
+        norm = LA.norm([diff_th,diff_ph])
+        th = th_new
+        ph = ph_new
+        count += 1
+        print(count)
+    
+    print("norm:")
+    print(norm)
+
+
+    fig2, ax2 = draw_base(xx,yy,zz,xv,yv,zv)
+    x,y,z = tor2cart(th,ph,c,a)
+    ax2.plot(x,y,z,c='g')
+
+    print(th)
+    print(ph)
+    
+    print("expected length:")
+    print(expected_length)
+
+    print("curve length:")
+    print(curve_length(th,ph))
+
     plt.show()
-
-    print(th_g)
-    print(ph_g)

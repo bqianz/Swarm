dda3de6133e5064e54ca7687433e41ed3e7c4da7
Beril Zhang <moujunki@gmail.com>
2019-10-31 21:05:38 -0700

    demployed on heroku

--- Torus/dash_demo.py
+++ Torus/dash_demo.py
@@ -29,6 +29,8 @@ for i in range(num_paths):
 
 app = dash.Dash(__name__)
 
+server = app.server
+
 app.layout = html.Div(children=[
     html.H1(children='Functional Iteration on Paths on the Torus'),
 
